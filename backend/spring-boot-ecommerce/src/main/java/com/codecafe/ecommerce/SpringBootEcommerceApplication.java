c17fbd094b25bc7c90eb25606e6eb5f8dc015a10
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-05-10 21:55:30 +0530

    initialized the spring-boot project

---
+++ backend/spring-boot-ecommerce/src/main/java/com/codecafe/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+}
