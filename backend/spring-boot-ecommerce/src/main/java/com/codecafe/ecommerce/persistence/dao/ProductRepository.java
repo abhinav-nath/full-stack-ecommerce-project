9fe3e39a79f136429f43c5ea6c30c261fe14e05a
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-05-12 01:40:28 +0530

    added a new query method to search products by name

--- backend/spring-boot-ecommerce/src/main/java/com/codecafe/ecommerce/persistence/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/codecafe/ecommerce/persistence/dao/ProductRepository.java
@@ -8,12 +8,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.codecafe.ecommerce.persistence.entity.Product;
 
+// Spring Data REST auto-magically creates SQL queries on the basis of the method names 
+
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
+
     // Spring Data REST automatically exposes endpoint - http://host:port/api/products/search/findByCategoryId?id=:id
-    
+
     // SELECT * FROM product WHERE category_id = ?
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    // SELECT * FROM product p WHERE p.name LIKE CONCAT('%', :name, '%')
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
 }
