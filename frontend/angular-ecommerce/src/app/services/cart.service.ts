fdc8a50459f4d95b7d332b8839d8852cd2f504f2
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-05-18 21:49:41 +0530

    created CartService to publish events for totalPrice and totalQuantity

---
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../model/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  //Subject is a subclass of Observable. We can use Subject to publish events. The event will be sent to all of the subscribers
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+
+  constructor() { }
+
+
+  addToCart(theCartItem: CartItem) {
+
+    // check if we already have the item in our cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+
+    if (this.cartItems.length > 0) {
+
+      // find the item in the cart based on the item id
+      for (let cartItem of this.cartItems) {
+        if (cartItem.id === theCartItem.id) {
+          existingCartItem = theCartItem;
+          break;
+        }
+      }
+
+      // check if we found it
+      alreadyExistsInCart = (existingCartItem != undefined);
+
+    }
+
+    if (alreadyExistsInCart) {
+      // increment the quantity
+      existingCartItem.quantity++;
+    } else {
+      // just add the item to the array
+      this.cartItems.push(theCartItem);
+    }
+
+    // compute cart total price and total quantity
+    this.computeCartTotals();
+
+  }
+
+
+  computeCartTotals() {
+
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems) {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+
+    // publish the new values, all subscribers will receive the new data
+
+    // this will publish two events to all subscribers
+    // one event for totalPrice, one event for totalQuantity
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    // log the cart data for debugging purposes
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+
+    console.log("Contents of the cart");
+    for (let cartItem of this.cartItems) {
+      const subTotalPrice = cartItem.quantity * cartItem.unitPrice;
+      console.log(`name: ${cartItem.name}, quantity: ${cartItem.quantity}, unitPrice: ${cartItem.unitPrice}, subTotalPrice: ${subTotalPrice}`);
+    }
+
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totatQuantity: ${totalQuantityValue}`);
+    console.log('----');
+  }
+
+}
