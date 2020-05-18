d70386914057cd37817c28edc6719a6778239717
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-05-18 22:41:10 +0530

    CartService - refactored for loop to lambda expression

--- frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -26,12 +26,7 @@ export class CartService {
     if (this.cartItems.length > 0) {
 
       // find the item in the cart based on the item id
-      for (let cartItem of this.cartItems) {
-        if (cartItem.id === theCartItem.id) {
-          existingCartItem = cartItem;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find(cartItem => cartItem.id === theCartItem.id);
 
       // check if we found it
       alreadyExistsInCart = (existingCartItem != undefined);
