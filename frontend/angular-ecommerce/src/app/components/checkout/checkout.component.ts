b861a7c8c2dafda8306aef7f1fa05d65f388548c
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-05-19 02:26:07 +0530

    added shipping address and billing address form groups

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -19,14 +19,48 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
       })
     });
   }
+
 
   onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
     //console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+  }
+
+
+  copyShippingAddressToBillingAddress(event) {
+
+    if(event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress.setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    } else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+    }
+
   }
 
 }
