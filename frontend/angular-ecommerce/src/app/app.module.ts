ae3ad81e70844e5222b0602cfdb009c10fa744f3
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2020-05-18 21:04:29 +0530

    created CartStatusComponent and added HTML template to it

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -12,7 +12,8 @@ import { ProductCategoryMenuComponent } from './components/product-category-menu
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component'
 
 const routes: Routes = [
   { path: 'products/:id', component: ProductDetailsComponent },
@@ -30,7 +31,8 @@ const routes: Routes = [
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartStatusComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
---
+++ frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-status',
+  templateUrl: './cart-status.component.html',
+  styleUrls: ['./cart-status.component.css']
+})
+export class CartStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
